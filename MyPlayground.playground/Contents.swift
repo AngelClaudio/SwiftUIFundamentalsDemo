dca77fbad129e5ca05a966188ffdb36c91d354c2
Angel <angelclaudio@outlook.com>
2024-02-11 22:43:42 -0500

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+struct Employee {
+    var name: String
+    var vacationAllocated = 14
+    var vacationTaken = 0
+    
+    var vacationRemaining: Int{
+        get{
+            vacationAllocated - vacationTaken
+        }
+        
+        set{
+            vacationAllocated = vacationTaken + newValue
+        }
+    }
+}
+
+var angelEmployee = Employee(name: "Angel")
+angelEmployee.vacationTaken += 4 // vacationTaken now is 4
+angelEmployee.vacationRemaining = 5 // vacationAllocated now equals 9
+print(angelEmployee.vacationAllocated) // 9 - 4, which equals 5
+
