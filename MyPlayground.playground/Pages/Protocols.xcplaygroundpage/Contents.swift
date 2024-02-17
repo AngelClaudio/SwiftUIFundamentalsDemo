24f545b523727948188c9ed1742a12ba33909c3c
Angel <angelclaudio@outlook.com>
2024-02-16 20:33:42 -0500

    Added files

--- MyPlayground.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,7 +1,40 @@
-//: [Previous](@previous)
+// Protocol Example
+protocol Vehicle{
+    var name: String {get}
+    var currentPassengers: Int { get set }
+    
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
 
-import Foundation
+// Adopting aka Conforming to protocol
+struct Car: Vehicle {
+    var name: String
+    
+    var currentPassengers: Int
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+func commute(distance: Int, using vehicle: Vehicle){
+    if vehicle.estimateTime(for: distance)>100{
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+// Example using the struct that conformed to Vehicle
+let car = Car(name: "lambo", currentPassengers: 2)
+commute(distance: 100, using: car)
+
--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -21,26 +21,5 @@ angelEmployee.vacationTaken += 4 // vacationTaken now is 4
 angelEmployee.vacationRemaining = 5 // vacationAllocated now equals 9
 print(angelEmployee.vacationAllocated) // 9 - 4, which equals 5
 
-// Protocol Example
 
-protocol Vehicle{
-    var name: String {get}
-    var currentPassengers: Int { get set }
-    
-    func estimateTime(for distance: Int) -> Int
-    func travel(distance: Int)
-}
 
-struct Car: Vehicle {
-    var name: String
-    
-    var currentPassengers: Int
-    
-    func estimateTime(for distance: Int) -> Int {
-        distance / 50
-    }
-    
-    func travel(distance: Int) {
-        print("I'm driving \(distance)km")
-    }
-}
